384b977297f63942880b681541823b4e21d14b78
Michael <m051412369811@gmail.com>
2025-06-25 05:09:42 +0800

    CORS設定變更

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -20,7 +20,7 @@ public class DemoApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/api/**") // 你的 API 路徑
-						.allowedOrigins("http://localhost:5173") // 你的前端網址
+						.allowedOrigins("https://m051412369811.github.io", "http://localhost:5173") // 你的前端網址
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowCredentials(true) // 這行一定要加，才能 session-based login
 						.allowedHeaders("*")
